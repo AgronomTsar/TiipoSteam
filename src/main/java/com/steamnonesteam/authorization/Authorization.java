55558bb40eb5f813b853984294eabc7498fb9e2f
Agronom <Utarbaev2003@mail.ru>
2021-02-15 19:00:03 +0600

    first commit

--- src/main/java/com/steamnonesteam/authorization/Authorization.java
+++ src/main/java/com/steamnonesteam/authorization/Authorization.java
@@ -5,63 +5,79 @@ import com.steamnonesteam.model.UserGame;
 import io.javalin.core.security.BasicAuthCredentials;
 import io.javalin.http.Context;
 import org.mindrot.jbcrypt.BCrypt;
-
 import java.sql.SQLException;
 import java.util.ArrayList;
-
 public class Authorization{
     private Dao<User, Integer> dao;
     private Dao<UserGame,Integer> libraryDao;
+    final String nickname="nickname";
+    final String admin="Admin";
+    final String userString="user";
+    final String users="users";
+    final String idString="id";
+    final String gameId="gameId";
+    final String library="Library";
+    final int notHere=-1;
+    final int userChecker=2;
+    final int libraryChecker=3;
+    final int zero=0;
     public Authorization(Dao<User, Integer> dao,Dao<UserGame,Integer> libraryDao)
     {
         this.dao = dao;
         this.libraryDao=libraryDao;
+    }
+    public Roles getUserRole(Context ctx,User user){
+        String path=ctx.path();
+        if(path.indexOf(users)!=notHere) {
+            int check = (int) path.chars().filter(ch -> ch == '/').count();
+            if (check == userChecker) {
+                if (Integer.parseInt(ctx.pathParam(idString)) == user.getId()) {
+                    return Roles.USER;
+                } else {
+                    return null;
+                }
+            } else {
+                return Roles.USER;
+            }
+        }
+        return null;
+    }
+    public Roles libraryChecker(Context ctx,User user) throws SQLException {
+        String path=ctx.path();
+        int checkLibrary=(int) path.chars().filter(ch -> ch == '/').count();
+        if(checkLibrary==libraryChecker){
+            if(Integer.parseInt(ctx.pathParam(userString))==user.getId()){
+                ArrayList<UserGame> library= (ArrayList<UserGame>) libraryDao.queryForEq(gameId,Integer.parseInt(ctx.pathParam("game")));
+                int userId=Integer.parseInt(ctx.pathParam(userString));
+                for(int i=zero;i<=library.size();i++){
+                    if(userId==library.get(i).getUserId().getId()){
+                        return Roles.USER;
+                    }
+                }
+            }
+        }
+        else{
+            return Roles.USER;
+        }
+        return  null;
     }
     public Roles getUserRole(Context ctx) throws SQLException {
         if(ctx.basicAuthCredentials().getUsername().equals("")){
             return Roles.USER;
         }
         BasicAuthCredentials loginPassword=ctx.basicAuthCredentials();
-        User user=dao.queryForEq("nickname",loginPassword.getUsername()).get(0);
+        User user=dao.queryForEq(nickname,loginPassword.getUsername()).get(0);
         if(BCrypt.checkpw(loginPassword.getPassword(), user.getPassword())){
-            if(user.getRole().equals("Admin")){
-//            System.out.println("Admin");
+            if(user.getRole().equals(admin)){
                 return Roles.ADMIN;
             }
-            else if(user.getRole().equals("user")){
+            else if(user.getRole().equals(userString)){
                 String path=ctx.path();
-                if(path.indexOf("users")!=-1){
-                    int check= (int) path.chars().filter(ch -> ch == '/').count();
-                   if(check==2){
-                       if(Integer.parseInt(ctx.pathParam("id"))==user.getId()){
-                           return Roles.USER;
-                       }
-                       else {
-                           return null;
-                       }
-                   }
-                   else {
-                       return Roles.USER;
-                   }
+                if(path.indexOf(users)!=notHere){
+                    return getUserRole(ctx,user);
                 }
-                else if(path.indexOf("Library")!=-1){
-                    int checkLibrary=(int) path.chars().filter(ch -> ch == '/').count();
-                    System.out.println("work");
-                    if(checkLibrary==3){
-                        if(Integer.parseInt(ctx.pathParam("user"))==user.getId()){
-                            ArrayList<UserGame> library= (ArrayList<UserGame>) libraryDao.queryForEq("gameId",Integer.parseInt(ctx.pathParam("game")));
-                            int userId=Integer.parseInt(ctx.pathParam("user"));
-                            for(int i=0;i<=library.size();i++){
-                                if(userId==library.get(i).getUserId().getId()){
-                                    return Roles.USER;
-                                }
-                            }
-
-                        }
-                    }
-                    else{
-                        return Roles.USER;
-                    }
+                else if(path.indexOf(library)!=notHere){
+                    return libraryChecker(ctx,user);
                 }
             }
             else {
